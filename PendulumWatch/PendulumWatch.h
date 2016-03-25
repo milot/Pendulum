6af39548a5d75b3b27ee5db7882ba1f342d36f5c
milot <milot.shala@gmail.com>
2016-03-25 17:17:25 +0100

    Added support for tvOS and watchOS.

---
+++ PendulumWatch/PendulumWatch.h
@@ -0,0 +1,19 @@
+//
+//  PendulumWatch.h
+//  PendulumWatch
+//
+//  Created by Milot Shala on 3/25/16.
+//  Copyright © 2016 Milot Shala. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for PendulumWatch.
+FOUNDATION_EXPORT double PendulumWatchVersionNumber;
+
+//! Project version string for PendulumWatch.
+FOUNDATION_EXPORT const unsigned char PendulumWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PendulumWatch/PublicHeader.h>
+
+
